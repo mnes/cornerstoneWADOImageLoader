8f4b1ca9c7c4088670bf06b3ba3aefc4f745222a
kambayashi <81724536+kambayashi-r@users.noreply.github.com>
2024-08-21 11:59:59 +0900

    try-catch an exception then call reject() in promise to handle the error (#1)
    * Add: .node-version(16.17.1)
    
    * Add: try-catch and reject error
    
    * Add: try-catch and reject error

--- src/imageLoader/createImage.js
+++ src/imageLoader/createImage.js
@@ -90,204 +90,208 @@ function createImage(imageId, pixelData, transferSyntax, options = {}) {
   return new Promise((resolve, reject) => {
     // eslint-disable-next-line complexity
     decodePromise.then(function(imageFrame) {
-      // If we have a target buffer that was written to in the
-      // Decode task, point the image to it here.
-      // We can't have done it within the thread incase it was a SharedArrayBuffer.
-      let alreadyTyped = false;
+      try {
+        // If we have a target buffer that was written to in the
+        // Decode task, point the image to it here.
+        // We can't have done it within the thread incase it was a SharedArrayBuffer.
+        let alreadyTyped = false;
 
-      if (options.targetBuffer) {
-        let offset, length;
-        // If we have a target buffer, write to that instead. This helps reduce memory duplication.
+        if (options.targetBuffer) {
+          let offset, length;
+          // If we have a target buffer, write to that instead. This helps reduce memory duplication.
 
-        ({ offset, length } = options.targetBuffer);
-        const { arrayBuffer, type } = options.targetBuffer;
+          ({ offset, length } = options.targetBuffer);
+          const { arrayBuffer, type } = options.targetBuffer;
 
-        let TypedArrayConstructor;
+          let TypedArrayConstructor;
 
-        if (length === null || length === undefined) {
-          length = imageFrame.pixelDataLength;
-        }
-
-        if (offset === null || offset === undefined) {
-          offset = 0;
-        }
-
-        switch (type) {
-          case 'Uint8Array':
-            TypedArrayConstructor = Uint8Array;
-            break;
-          case 'Uint16Array':
-            TypedArrayConstructor = Uint16Array;
-            break;
-          case 'Float32Array':
-            TypedArrayConstructor = Float32Array;
-            break;
-          default:
-            throw new Error(
-              'target array for image does not have a valid type.'
-            );
-        }
-
-        if (length !== imageFrame.pixelDataLength) {
-          throw new Error(
-            'target array for image does not have the same length as the decoded image length.'
-          );
-        }
-
-        // TypedArray.Set is api level and ~50x faster than copying elements even for
-        // Arrays of different types, which aren't simply memcpy ops.
-        let typedArray;
-
-        if (arrayBuffer) {
-          typedArray = new TypedArrayConstructor(arrayBuffer, offset, length);
-        } else {
-          typedArray = new TypedArrayConstructor(imageFrame.pixelData);
-        }
-
-        // If need to scale, need to scale correct array.
-        imageFrame.pixelData = typedArray;
-        alreadyTyped = true;
-      }
-
-      const imagePlaneModule =
-        cornerstone.metaData.get('imagePlaneModule', imageId) || {};
-      const voiLutModule =
-        cornerstone.metaData.get('voiLutModule', imageId) || {};
-      const modalityLutModule =
-        cornerstone.metaData.get('modalityLutModule', imageId) || {};
-      const sopCommonModule =
-        cornerstone.metaData.get('sopCommonModule', imageId) || {};
-      const isColorImage = isColorImageFn(imageFrame.photometricInterpretation);
-
-      // JPEGBaseline (8 bits) is already returning the pixel data in the right format (rgba)
-      // because it's using a canvas to load and decode images.
-      if (!isJPEGBaseline8BitColor(imageFrame, transferSyntax)) {
-        if (!alreadyTyped) {
-          setPixelDataType(imageFrame);
-        }
-
-        // convert color space
-        if (isColorImage) {
-          // setup the canvas context
-          canvas.height = imageFrame.rows;
-          canvas.width = imageFrame.columns;
-
-          const context = canvas.getContext('2d');
-          const imageData = context.createImageData(
-            imageFrame.columns,
-            imageFrame.rows
-          );
-
-          convertColorSpace(imageFrame, imageData);
-          imageFrame.imageData = imageData;
-          imageFrame.pixelData = imageData.data;
-
-          // calculate smallest and largest PixelValue of the converted pixelData
-          const minMax = getMinMax(imageFrame.pixelData);
-
-          imageFrame.smallestPixelValue = minMax.min;
-          imageFrame.largestPixelValue = minMax.max;
-        }
-      }
-
-      const image = {
-        imageId,
-        color: isColorImage,
-        columnPixelSpacing: imagePlaneModule.columnPixelSpacing,
-        columns: imageFrame.columns,
-        height: imageFrame.rows,
-        intercept: modalityLutModule.rescaleIntercept
-          ? modalityLutModule.rescaleIntercept
-          : 0,
-        invert: imageFrame.photometricInterpretation === 'MONOCHROME1',
-        minPixelValue: imageFrame.smallestPixelValue,
-        maxPixelValue: imageFrame.largestPixelValue,
-        rowPixelSpacing: imagePlaneModule.rowPixelSpacing,
-        rows: imageFrame.rows,
-        sizeInBytes: imageFrame.pixelData.byteLength,
-        slope: modalityLutModule.rescaleSlope
-          ? modalityLutModule.rescaleSlope
-          : 1,
-        width: imageFrame.columns,
-        windowCenter: voiLutModule.windowCenter
-          ? voiLutModule.windowCenter[0]
-          : undefined,
-        windowWidth: voiLutModule.windowWidth
-          ? voiLutModule.windowWidth[0]
-          : undefined,
-        decodeTimeInMS: imageFrame.decodeTimeInMS,
-        floatPixelData: undefined,
-        imageFrame,
-      };
-
-      // If pixel data is intrinsically floating 32 array, we convert it to int for
-      // display in cornerstone. For other cases when pixel data is typed as
-      // Float32Array for scaling; this conversion is not needed.
-      if (
-        imageFrame.pixelData instanceof Float32Array &&
-        convertFloatPixelDataToInt
-      ) {
-        const floatPixelData = imageFrame.pixelData;
-        const results = convertToIntPixelData(floatPixelData);
-
-        image.minPixelValue = results.min;
-        image.maxPixelValue = results.max;
-        image.slope = results.slope;
-        image.intercept = results.intercept;
-        image.floatPixelData = floatPixelData;
-        image.getPixelData = () => results.intPixelData;
-      } else {
-        image.getPixelData = () => imageFrame.pixelData;
-      }
-
-      if (image.color) {
-        image.getCanvas = function() {
-          if (lastImageIdDrawn === imageId) {
-            return canvas;
+          if (length === null || length === undefined) {
+            length = imageFrame.pixelDataLength;
           }
 
-          canvas.height = image.rows;
-          canvas.width = image.columns;
-          const context = canvas.getContext('2d');
+          if (offset === null || offset === undefined) {
+            offset = 0;
+          }
 
-          context.putImageData(imageFrame.imageData, 0, 0);
-          lastImageIdDrawn = imageId;
+          switch (type) {
+            case 'Uint8Array':
+              TypedArrayConstructor = Uint8Array;
+              break;
+            case 'Uint16Array':
+              TypedArrayConstructor = Uint16Array;
+              break;
+            case 'Float32Array':
+              TypedArrayConstructor = Float32Array;
+              break;
+            default:
+              throw new Error(
+                'target array for image does not have a valid type.'
+              );
+          }
 
-          return canvas;
+          if (length !== imageFrame.pixelDataLength) {
+            throw new Error(
+              'target array for image does not have the same length as the decoded image length.'
+            );
+          }
+
+          // TypedArray.Set is api level and ~50x faster than copying elements even for
+          // Arrays of different types, which aren't simply memcpy ops.
+          let typedArray;
+
+          if (arrayBuffer) {
+            typedArray = new TypedArrayConstructor(arrayBuffer, offset, length);
+          } else {
+            typedArray = new TypedArrayConstructor(imageFrame.pixelData);
+          }
+
+          // If need to scale, need to scale correct array.
+          imageFrame.pixelData = typedArray;
+          alreadyTyped = true;
+        }
+
+        const imagePlaneModule =
+          cornerstone.metaData.get('imagePlaneModule', imageId) || {};
+        const voiLutModule =
+          cornerstone.metaData.get('voiLutModule', imageId) || {};
+        const modalityLutModule =
+          cornerstone.metaData.get('modalityLutModule', imageId) || {};
+        const sopCommonModule =
+          cornerstone.metaData.get('sopCommonModule', imageId) || {};
+        const isColorImage = isColorImageFn(imageFrame.photometricInterpretation);
+
+        // JPEGBaseline (8 bits) is already returning the pixel data in the right format (rgba)
+        // because it's using a canvas to load and decode images.
+        if (!isJPEGBaseline8BitColor(imageFrame, transferSyntax)) {
+          if (!alreadyTyped) {
+            setPixelDataType(imageFrame);
+          }
+
+          // convert color space
+          if (isColorImage) {
+            // setup the canvas context
+            canvas.height = imageFrame.rows;
+            canvas.width = imageFrame.columns;
+
+            const context = canvas.getContext('2d');
+            const imageData = context.createImageData(
+              imageFrame.columns,
+              imageFrame.rows
+            );
+
+            convertColorSpace(imageFrame, imageData);
+            imageFrame.imageData = imageData;
+            imageFrame.pixelData = imageData.data;
+
+            // calculate smallest and largest PixelValue of the converted pixelData
+            const minMax = getMinMax(imageFrame.pixelData);
+
+            imageFrame.smallestPixelValue = minMax.min;
+            imageFrame.largestPixelValue = minMax.max;
+          }
+        }
+
+        const image = {
+          imageId,
+          color: isColorImage,
+          columnPixelSpacing: imagePlaneModule.columnPixelSpacing,
+          columns: imageFrame.columns,
+          height: imageFrame.rows,
+          intercept: modalityLutModule.rescaleIntercept
+            ? modalityLutModule.rescaleIntercept
+            : 0,
+          invert: imageFrame.photometricInterpretation === 'MONOCHROME1',
+          minPixelValue: imageFrame.smallestPixelValue,
+          maxPixelValue: imageFrame.largestPixelValue,
+          rowPixelSpacing: imagePlaneModule.rowPixelSpacing,
+          rows: imageFrame.rows,
+          sizeInBytes: imageFrame.pixelData.byteLength,
+          slope: modalityLutModule.rescaleSlope
+            ? modalityLutModule.rescaleSlope
+            : 1,
+          width: imageFrame.columns,
+          windowCenter: voiLutModule.windowCenter
+            ? voiLutModule.windowCenter[0]
+            : undefined,
+          windowWidth: voiLutModule.windowWidth
+            ? voiLutModule.windowWidth[0]
+            : undefined,
+          decodeTimeInMS: imageFrame.decodeTimeInMS,
+          floatPixelData: undefined,
+          imageFrame,
         };
-      }
 
-      // Modality LUT
-      if (
-        modalityLutModule.modalityLUTSequence &&
-        modalityLutModule.modalityLUTSequence.length > 0 &&
-        isModalityLUTForDisplay(sopCommonModule.sopClassUID)
-      ) {
-        image.modalityLUT = modalityLutModule.modalityLUTSequence[0];
-      }
+        // If pixel data is intrinsically floating 32 array, we convert it to int for
+        // display in cornerstone. For other cases when pixel data is typed as
+        // Float32Array for scaling; this conversion is not needed.
+        if (
+          imageFrame.pixelData instanceof Float32Array &&
+          convertFloatPixelDataToInt
+        ) {
+          const floatPixelData = imageFrame.pixelData;
+          const results = convertToIntPixelData(floatPixelData);
 
-      // VOI LUT
-      if (
-        voiLutModule.voiLUTSequence &&
-        voiLutModule.voiLUTSequence.length > 0
-      ) {
-        image.voiLUT = voiLutModule.voiLUTSequence[0];
-      }
+          image.minPixelValue = results.min;
+          image.maxPixelValue = results.max;
+          image.slope = results.slope;
+          image.intercept = results.intercept;
+          image.floatPixelData = floatPixelData;
+          image.getPixelData = () => results.intPixelData;
+        } else {
+          image.getPixelData = () => imageFrame.pixelData;
+        }
 
-      if (image.color) {
-        image.windowWidth = 255;
-        image.windowCenter = 127;
-      }
+        if (image.color) {
+          image.getCanvas = function() {
+            if (lastImageIdDrawn === imageId) {
+              return canvas;
+            }
 
-      // set the ww/wc to cover the dynamic range of the image if no values are supplied
-      if (image.windowCenter === undefined || image.windowWidth === undefined) {
-        const maxVoi = image.maxPixelValue * image.slope + image.intercept;
-        const minVoi = image.minPixelValue * image.slope + image.intercept;
+            canvas.height = image.rows;
+            canvas.width = image.columns;
+            const context = canvas.getContext('2d');
 
-        image.windowWidth = maxVoi - minVoi;
-        image.windowCenter = (maxVoi + minVoi) / 2;
+            context.putImageData(imageFrame.imageData, 0, 0);
+            lastImageIdDrawn = imageId;
+
+            return canvas;
+          };
+        }
+
+        // Modality LUT
+        if (
+          modalityLutModule.modalityLUTSequence &&
+          modalityLutModule.modalityLUTSequence.length > 0 &&
+          isModalityLUTForDisplay(sopCommonModule.sopClassUID)
+        ) {
+          image.modalityLUT = modalityLutModule.modalityLUTSequence[0];
+        }
+
+        // VOI LUT
+        if (
+          voiLutModule.voiLUTSequence &&
+          voiLutModule.voiLUTSequence.length > 0
+        ) {
+          image.voiLUT = voiLutModule.voiLUTSequence[0];
+        }
+
+        if (image.color) {
+          image.windowWidth = 255;
+          image.windowCenter = 127;
+        }
+
+        // set the ww/wc to cover the dynamic range of the image if no values are supplied
+        if (image.windowCenter === undefined || image.windowWidth === undefined) {
+          const maxVoi = image.maxPixelValue * image.slope + image.intercept;
+          const minVoi = image.minPixelValue * image.slope + image.intercept;
+
+          image.windowWidth = maxVoi - minVoi;
+          image.windowCenter = (maxVoi + minVoi) / 2;
+        }
+        resolve(image);
+      } catch (e) {
+        reject(e);
       }
-      resolve(image);
     }, reject);
   });
 }
